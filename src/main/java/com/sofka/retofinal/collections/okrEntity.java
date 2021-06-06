ec85c100573486adc4b9f1e3f3ad8dd31c5e5b9c
Mauricio Mancera <u20132121837@usco.edu.co>
2021-06-05 20:02:38 -0500

    se crea okrEntiry con todos sus atributos

---
+++ src/main/java/com/sofka/retofinal/collections/okrEntity.java
@@ -0,0 +1,94 @@
+package com.sofka.retofinal.collections;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class okrEntity {
+    @Id
+    private String id;
+    private String userId;
+    private String title;
+    private String object;
+    private String responName;
+    private String responEmail;
+    private String vertical;
+    private String description;
+
+
+    public okrEntity(String id, String userId, String title, String object, String responName,
+                     String responEmail, String vertical, String description) {
+        this.id = id;
+        this.userId = userId;
+        this.title = title;
+        this.object = object;
+        this.responName = responName;
+        this.responEmail = responEmail;
+        this.vertical = vertical;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    public String getResponName() {
+        return responName;
+    }
+
+    public void setResponName(String responName) {
+        this.responName = responName;
+    }
+
+    public String getResponEmail() {
+        return responEmail;
+    }
+
+    public void setResponEmail(String responEmail) {
+        this.responEmail = responEmail;
+    }
+
+    public String getVertical() {
+        return vertical;
+    }
+
+    public void setVertical(String vertical) {
+        this.vertical = vertical;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
